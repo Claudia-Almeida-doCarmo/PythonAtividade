d57e5c879f0c27464c2cf047b792f0daafab65ee
Claudia Almeida <108090945+Claudia-Almeida-doCarmo@users.noreply.github.com>
2022-07-31 00:00:16 -0300

    Add files via upload

---
+++ GitHub/Atividade1Github.py
@@ -0,0 +1,23 @@
+print('Luiz', type('Luiz'))
+print(10, type(10))
+print(25.23, type(25.23))
+print(10==10, type(10==10))
+print('l'=='l', type('l'== 'l'))
+print('C'=='l', type('c'== 'l'))
+print('1.10', type('1.10'))
+print(1.10, type(1.10))
+print(100, type(100))
+print('Luiz', type('Luiz'), bool('Luiz'))
+print('', type(''))
+print(bool(''))
+print('10', type('10'), type(int('10')))
+print('10',type('10'), type(10))
+print('10'+'10')
+# string: NOme
+print('Claudia Almeida', type('Claudia Almeida'))
+# Idade: int
+print(31, type(41))
+#Altura:Float
+print(1.60, type(1.60))
+#  É maior de idade
+print(31>18, type(31>18))
